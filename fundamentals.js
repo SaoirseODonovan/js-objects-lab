e4bb325a940a44a84135880acf133d8cc42e3feb
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-09-16 14:45:14 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -13,6 +13,28 @@
   
 //   console.log(aCar.owner + ' drives a ' + aCar.type.make);
 
+//Part 02
+// const aCar = {
+//   owner: "Joe Bloggs",
+//   type: {
+//     make: "Toyota",
+//     model: "Corolla",
+//     cc: 1.8
+//   },
+//   registration: { year: 201, county: "WD", number: 1058 }
+// };
+
+// console.log(aCar.owner + " drives a " + aCar.type.make);
+// console.log(
+//   "Reg. = " +
+//     aCar.registration.year +
+//     "-" +
+//     aCar.registration.county +
+//     "-" +
+//     aCar.registration.number
+// );
+
+//Part 3
 const aCar = {
   owner: "Joe Bloggs",
   type: {
@@ -25,10 +47,26 @@ const aCar = {
 
 console.log(aCar.owner + " drives a " + aCar.type.make);
 console.log(
-  "Reg. = " +
-    aCar.registration.year +
-    "-" +
-    aCar.registration.county +
-    "-" +
-    aCar.registration.number
+ "Reg. = " +
+   aCar.registration.year +
+   "-" +
+   aCar.registration.county +
+   "-" +
+   aCar.registration.number
+);
+
+aCar.milage = 10000
+aCar.color = {
+ exterior: "red",
+ interior: { texture: "leather", shade: "cream" }
+}
+
+console.log(
+ "It is a " +
+   aCar.color.exterior +
+   " car, " +
+   aCar.milage +
+   " milage, with " +
+   aCar.color.interior.texture +
+   " interior."
 );
